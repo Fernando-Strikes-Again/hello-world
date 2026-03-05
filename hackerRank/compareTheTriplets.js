d4046adc44d4459a43c76a4970e9e98a8a66bd53
Fernando-Strikes-Again <fernando.torres.j25@gmail.com>
2026-03-05 14:01:05 -0600

    added hackerRank exercises

---
+++ hackerRank/compareTheTriplets.js
@@ -0,0 +1,75 @@
+/*
+
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to calculate their comparison points by comparing each category:
+
+    If a[i] > b[i], then Alice is awarded 1 point.
+    If a[i] < b[i], then Bob is awarded 1 point.
+    If a[i] = b[i], then neither person receives a point.
+
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) {
+    let alicePoints = 0;
+    let bobPoints = 0;
+    
+    for(let i=0; i< a.length; i++){
+        if(a[i]>b[i]){
+            alicePoints++;
+            }else if(b[i]>a[i]){
+                bobPoints++;
+        }
+    }
+    return [alicePoints,bobPoints]
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
