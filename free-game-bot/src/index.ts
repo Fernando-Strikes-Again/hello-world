c93fd7fa924ac8c04823cf37a212c81cf4a620fd
Fernando-Strikes-Again <fernando.torres.j25@gmail.com>
2026-03-20 16:07:14 -0600

    Added Epic Scrapper

--- free-game-bot/src/index.ts
+++ free-game-bot/src/index.ts
@@ -1,21 +1,27 @@
 // tests/steam.spec.ts
 import { chromium } from 'playwright';
 import { SteamScraper } from '../src/SteamScraper';
+import { EpicScraper } from './EpicScraper';
 
 async function main() {
   console.log("--- BOTTING STARTING ---");
 
   const browser = await chromium.launch({ headless: false });
-  const page = await browser.newPage();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
   try {
+    // --- STEAM SECTION ---
+    console.log("Calling Steam scraper...");
     const steam = new SteamScraper(page);
+    const steamGames = await steam.scrape();
+    console.log("Steam Games found:", steamGames);
 
-    console.log("Calling scraper...");
-
-    const games = await steam.scrape();
-    
-    console.log("Games found:", games);
+    // --- EPIC SECTION ---
+    console.log("Calling Epic scraper...");
+    const epic = new EpicScraper(page);
+    const epicGames = await epic.scrape();
+    console.log("Epic Games found:", epicGames);
 
   } catch (error) {
     console.error("CRITICAL ERROR during execution:", error);
